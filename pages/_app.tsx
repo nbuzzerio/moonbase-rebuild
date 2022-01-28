7b349b98ebe3a74c7842187f20e97ed2a330ed47
Nick Buzzerio <nick.buzzerio@hotmail.com>
2022-01-27 23:46:18 -0500

    configured nextjs and tailwind

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.scss'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8'/>
+      </Head>
+      <Component {...pageProps} />                
+    </>
+  ) 
+}
+
+export default MyApp
